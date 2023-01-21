4133df4a2088e9ca920a07aa2021ab3ae15cfcfd
InyakaAbraham <abrahaminyaka@yahoo.com>
2023-01-21 14:30:21 +0100

    Refactored

--- UserService.Domain/DataContext/UserDataContext.cs
+++ UserService.Domain/DataContext/UserDataContext.cs
@@ -6,7 +6,6 @@ namespace UserService.Model.DataContext;
 public class UserDataContext : DbContext
 {
     public DbSet<User> User { get; set; }
-    public DbSet<IntegrationEvent> IntegrationEventOutbox { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseNpgsql(@"Server=127.0.0.1;Port=5433;Database=User;UserId=postgres;");
--- UserService.Domain/Model/IntegrationEvent.cs
+++
@@ -1,8 +0,0 @@
-namespace UserService.Model.Model;
-
-public class IntegrationEvent
-{
-    public int ID { get; set; }
-    public string Event { get; set; }
-    public string Data { get; set; }
-}
