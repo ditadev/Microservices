cd922d76ce458b9db33bb1109683178b0fcdab56
InyakaAbraham <abrahaminyaka@yahoo.com>
2023-01-21 14:29:34 +0100

    Refactored

---
+++ UserService.Domain/Model/IntegrationEvent.cs
@@ -0,0 +1,8 @@
+namespace UserService.Model.Model;
+
+public class IntegrationEvent
+{
+    public int ID { get; set; }
+    public string Event { get; set; }
+    public string Data { get; set; }
+}
